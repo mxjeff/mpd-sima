08d0003d11906338f32a54891edd13fe8316abcd
kaliko <kaliko@azylum.org>
2014-11-23 13:14:47 +0100

    Expose musicbrainz_albumartistid in track objects

--- sima/lib/track.py
+++ sima/lib/track.py
@@ -33,7 +33,7 @@ class Track:
 
     def __init__(self, file=None, time=0, pos=-1, **kwargs):
         self.title = self.artist = self.album = self.albumartist = ''
-        self.musicbrainz_artistid = None
+        self.musicbrainz_artistid = self.musicbrainz_albumartistid = None
         self.pos = int(pos)
         self._empty = False
         self._file = file
@@ -129,9 +129,12 @@ class Track:
     def get_artist(self):
        """Get artist object from track"""
        name = self.artist
-       if self.albumartist and self.albumartist != 'Various Artists':
+       mbid = self.musicbrainz_artistid
+       if self.albumartist != 'Various Artists':
            name = self.albumartist
-       return Artist(name=name, mbid=self.musicbrainz_artistid)
+       if self.musicbrainz_albumartistid != '89ad4ac3-39f7-470e-963a-56509c546377':
+           mbid = self.musicbrainz_albumartistid
+       return Artist(name=name, mbid=mbid)
 
 # VIM MODLINE
 # vim: ai ts=4 sw=4 sts=4 expandtab
