83d4cd0d2ac162f2f40547858da1e88a63e5c01f
kaliko <kaliko@azylum.org>
2015-10-26 14:30:18 +0100

    Update README and info

--- sima/info.py
+++ sima/info.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-MPD_sima is an non interactive MPD autoqueue client running in the background.
+MPD_sima is a non interactive MPD autoqueue client running in the background.
 It will queue new tracks chosen among artists similar to your current queued
 tracks, provided a title is found in your music library.
 Similar artists are fetched from last.fm or EchoNest webservice.
@@ -12,7 +12,7 @@ short.
 """
 
 
-__version__ = '0.14.0'
+__version__ = '0.14.1'
 __author__ = 'kaliko jack'
 __email__ = 'kaliko@azylum.org'
 __url__ = 'git://git.kaliko.me/sima.git'
