328d8ab76449eb0b045837bb5f79368af9df2fc1
kaliko <kaliko@azylum.org>
2017-02-20 11:42:51 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ classifiers = [
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         ]
 
 setup(name='MPD_sima',
