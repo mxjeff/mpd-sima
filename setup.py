69495ef9b6867b4fecf2026d883c52fdf1bbdb46
kaliko <kaliko@azylum.org>
2019-03-24 14:44:01 +0100

    Add python 3.7 to classifiers

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ classifiers = [
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         ]
 
 setup(name='MPD_sima',
