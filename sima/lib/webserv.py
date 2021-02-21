mxjeff/mpd-sima

commit baa6dc78770e02cfdaf79398a88b0a624a6836df

Some cleanup in log, get messages more uniform across plugins

sima/lib/webserv.py
@@ -327,7 +327,7 @@ class WebService(AdvancedPlugin):
             self.log.debug('Found no tracks to queue!')
             return
         for track in self.to_add:
-            self.log.info('%s candidates: %s', track, self.ws.name)
+            self.log.info('%s plugin chose: %s', self.ws.name, trk)
 
     def _album(self):
         """Get albums for album queue mode
@@ -356,6 +356,8 @@ class WebService(AdvancedPlugin):
         msg = ' '.join(['{0}: {1:>3d}'.format(k, v) for
                         k, v in sorted(self.ws.stats.items())])
         self.log.debug('http stats: ' + msg)
+        if not self.to_add:
+            self.log.info('%s plugin found nothing to queue', self.ws.name)
         candidates = self.to_add
         self.to_add = list()
         if self.plugin_conf.get('queue_mode') != 'album':

sima/lib/plugin.py
@@ -214,8 +214,8 @@ class AdvancedPlugin(Plugin):
         if not album_to_queue:
             self.log.info('No album found for "%s"', artist)
             return None
-        self.log.info('%s album candidate: %s - %s', self.__class__.__name__,
-                      artist, album_to_queue)
+        self.log.info('%s plugin chose album: %s - %s',
+                      self.__class__.__name__, artist, album_to_queue)
         return album_to_queue
 
     def filter_track(self, tracks, unplayed=False):

commit 5536f75085b68f1c538cb1cc0783b749fd0d4f74

Fixed variable in info log message (typo introduced in baa6dc7)

sima/lib/webserv.py
@@ -327,7 +327,7 @@ class WebService(AdvancedPlugin):
             self.log.debug('Found no tracks to queue!')
             return
         for track in self.to_add:
-            self.log.info('%s plugin chose: %s', self.ws.name, trk)
+            self.log.info('%s plugin chose: %s', self.ws.name, track)
 
     def _album(self):
         """Get albums for album queue mode