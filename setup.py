50deaf44207ccd6ac60deed4ca2af54c15bb3be2
kaliko <kaliko@azylum.org>
2021-04-20 10:42:02 +0200

    setup.py: Remove useless import

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import glob
 
 from setuptools import setup, find_packages  # >= 2.0.2
 from os import listdir
-from os.path import isfile, isdir
+from os.path import isfile
 
 from sima.info import __version__ as VERSION, __author__ as AUTHOR
 from sima.info import __doc__ as DESCRIPTION, __email__ as EMAIL
