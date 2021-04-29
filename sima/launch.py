mxjeff/mpd-sima

commit 6194c3ae982cf2a89044b7dc077dfb9915e5b106

Removed obsolete CLI options

sima/launch.py
@@ -118,24 +118,6 @@ def start(sopt, restart=False):
             SimaDB(db_path=db_file).purge_history(duration=0)
             sys.exit(0)
 
-
-    # TODO: To remove eventually in next major realese v0.18
-    # Create Database
-    db_file = config.get('sima', 'db_file')
-    if (sopt.options.get('create_db', None)
-            or not isfile(db_file)):
-        logger.info('Creating database in "%s"', db_file)
-        open(db_file, 'a').close()
-        SimaDB(db_path=db_file).create_db()
-        if sopt.options.get('create_db', None):
-            logger.info('Done, bye...')
-        sys.exit(0)
-
-    # TODO: To remove eventually in next major realese v0.18
-    if sopt.options.get('generate_config'):
-        config.write(sys.stdout, space_around_delimiters=True)
-        sys.exit(0)
-
     logger.info('Starting (%s)...', info.__version__)
     sima = core.Sima(config)
 

sima/utils/startopt.py
@@ -94,21 +94,11 @@ OPTS = [
         'action': Rfile,
         'metavar': 'CONFIG',
         'help': 'configuration file to load'},
-    {  # TODO: To remove eventually in next major realese v0.18
-        'sw': ['--generate-config'],
-        'dest': 'generate_config',
-        'action': 'store_true',
-        'help': SUPPRESS},
     {
         'sw': ['--var-dir', '--var_dir'],
         'dest': 'var_dir',
         'action': Wdir,
         'help': 'directory to store var content (ie. database, cache)'},
-    {  # TODO: To remove eventually in next major realese v0.18
-        'sw': ['--create-db'],
-        'action': 'store_true',
-        'dest': 'create_db',
-        'help': SUPPRESS},
     {
         'sw': ['command'],
         'nargs': '?',

commit bd2085416b7e7e52dec62f872ad87ea4cadb05f2

Fixed bug introduced in 6194c3a
DB was not created when missing on startup.

sima/launch.py
@@ -88,6 +88,13 @@ def start(sopt, restart=False):
     set_logger(verbosity, logfile)
     logger.debug('Command line say: %s', sopt.options)
 
+    # Create database if not present
+    db_file = config.get('sima', 'db_file')
+    if not isfile(db_file):
+        logger.debug('Creating database in "%s"', db_file)
+        open(db_file, 'a').close()  # TODO: to remove with new simadb in v0.18
+        SimaDB(db_path=db_file).create_db()
+
     if sopt.options.get('command'):
         cmd = sopt.options.get('command')
         if cmd == "generate-config":
@@ -100,10 +107,9 @@ def start(sopt, restart=False):
             config_test(config)
             sys.exit(0)
         if cmd == "create-db":
-            db_file = config.get('sima', 'db_file')
             if not isfile(db_file):
                 logger.info('Creating database in "%s"', db_file)
-                open(db_file, 'a').close()
+                open(db_file, 'a').close()  # TODO: to remove with new simadb in v0.18
                 SimaDB(db_path=db_file).create_db()
                 if sopt.options.get('create_db', None):
                     logger.info('Done, bye...')