e4f642e20348dab3523316157dafdd609fa32455
kaliko <kaliko@azylum.org>
2024-02-09 16:14:53 +0100

    Fixed sphinx build

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -17,6 +17,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
 from sima.info import __author__
 
 # -- General configuration ------------------------------------------------
